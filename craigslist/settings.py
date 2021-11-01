8ffea63935be4079694a3988b8430861242c0251
mohsen-ameli <mohsen.ameli83@gmail.com>
2021-10-31 22:48:09 -0400

    secret key hiden now

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u8e%@&18eth1$+&kqf77=)1)bi*1mbm(@wt-rs3@q$%hj#9k&4"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
